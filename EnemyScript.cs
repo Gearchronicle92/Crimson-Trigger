f161c7be5d269235350a6ddc48864d777aea510c
Gearchronicle92 <Madisonmark662@gmail.com>
2016-05-09 19:18:09 -0400

    Add files via upload

---
+++ EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+
+	int enemyHealth; //what is this for?
+
+
+	// Use this for initialization
+	void Start () {
+		enemyHealth  = 10;
+		
+
+	}
+
+	void DeductPoints (int DamageAmount)  { //what does this function do?
+		enemyHealth -= DamageAmount; //what is happening here?
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (enemyHealth <= 0) { //what condition is being checked?
+			Destroy (gameObject); //what does this do?
+		}
+
+	}
+}
